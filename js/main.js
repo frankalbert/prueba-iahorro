4782b25c3d66f73a77799196748933edd0921f11
Frank <frank.lora.93@gmail.com>
2021-08-20 14:38:01 +0200

    Añadir nuevas lógicas y ver todas las casuísticas

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ let playerWinner = null;
 
 const boards = document.querySelectorAll(".board");
 
-
 function resetAllLetDependencies() {
     state = [
         [null, null, null],
@@ -25,24 +24,35 @@ function populate(board) {
     updateBoard(board);
 }
 
-function nextPlayer(board) {
+// Función que cambia el svg del Header
+function updateTitleBoard(board) {
     const title = board.querySelector(".board__header .title");
-    const titleSpan = board.querySelector(".board__header .title span");
-    const titleImage = board.querySelector(".board__header .title img");
-    titleSpan.textContent = "Turno de: ";
-    let image = document.createElement("img");
+    const titleSvg = board.querySelector(".board__header .title svg");
+    const svgX = board.querySelector('.board__footer--item .icon.x').cloneNode(true);
+    const svgCircle = board.querySelector('.board__footer--item .icon.o').cloneNode(true);
+
+    let svg = "";
 
     if (!isTurnX) {
-        image.src = `${pathImage}o${colorPrimary ? "" : "2"}.png`;
+        svg = svgCircle;
     } else {
-        image.src = `${pathImage}x${colorPrimary ? "" : "2"}.png`;
+        svg = svgX;
     }
 
-    if (titleImage) {
-        title.removeChild(titleImage);
+    if (titleSvg) {
+        title.removeChild(titleSvg);
     }
-    title.appendChild(image);
 
+    title.append(svg);
+}
+
+function nextPlayer(board) {
+
+    const titleSpan = board.querySelector(".board__header .title span");
+    titleSpan.textContent = "Turno de: ";
+
+    // Cambiando el svg del Header
+    updateTitleBoard(board);
     updateBoard(board);
     findWinner(board);
 }
@@ -119,6 +129,10 @@ function findWinner(board) {
             positionInitial: 4,
             offset: 1,
         },
+        {
+            positionInitial: 5,
+            offset: 1,
+        },
     ];
 
     for (let i = 0; i < vectorToSearch.length; i++) {
@@ -131,7 +145,6 @@ function findWinner(board) {
 
     if (playerWinner) {
         const titleSpan = board.querySelector(".board__header .title span");
-        const titleImage = board.querySelector(".board__header .title img");
         const puntuacion = JSON.parse(sessionStorage.getItem('puntuacion') ? sessionStorage.getItem('puntuacion') : JSON.stringify({
             'x': 0,
             'o': 0,
@@ -141,13 +154,11 @@ function findWinner(board) {
 
         titleSpan.textContent = "Ha ganado: ";
 
-        if (titleImage) {
-            if (isTurnX) {
-                titleImage.src = `${pathImage}o${colorPrimary ? "" : "2"}.png`;
-            } else {
-                titleImage.src = `${pathImage}x${colorPrimary ? "" : "2"}.png`;
-            }
-        }
+        // Devolvemos el ganador a la posición anterior
+        isTurnX = !isTurnX;
+
+        // Cambiando el svg del Header
+        updateTitleBoard(board);
 
         board.querySelector('.nuevo__juego').removeAttribute('disabled');
         sessionStorage.setItem('puntuacion', JSON.stringify(puntuacion));
@@ -166,7 +177,11 @@ function updateStateByPosition(positionX, positionY, value) {
 }
 
 function updateBoard(board) {
+
     board.querySelector(".board__body").innerHTML = "";
+    const svgX = board.querySelector('.board__footer--item .icon.x').cloneNode(true);
+    const svgCircle = board.querySelector('.board__footer--item .icon.o').cloneNode(true);
+
     state.forEach((row, index) => {
         let newRow = document.createElement("div");
         newRow.classList.add(
@@ -182,21 +197,19 @@ function updateBoard(board) {
             const newCol = document.createElement("div");
             newCol.classList.add("col", "no-pading");
 
-            let urlImage = "";
+            let svg = "";
             let classPlayerClicked = "";
 
             if (value === "x") {
-                urlImage = `${pathImage}x${colorPrimary ? "" : "2"}.png`;
+                svg = svgX.outerHTML;
                 classPlayerClicked = `x ${colorPrimary ? '' : 'secundary'}`;
             } else if (value === "o") {
-                urlImage = `${pathImage}o${colorPrimary ? "" : "2"}.png`;
+                svg = svgCircle.outerHTML;
                 classPlayerClicked = `o ${colorPrimary ? '' : 'secundary'}`;
             }
 
-            newCol.innerHTML = `<div class="box d__flex justify-center align-center ${classPlayerClicked}" data-position-x="${index}" data-position-y="${i}">
-                  ${urlImage ? `<img src="${urlImage}" alt="imagen icono" />` : ""
-                }
-              </div>`;
+            newCol.innerHTML = `<div class="box d__flex justify-center align-center ${classPlayerClicked}" data-position-x="${index}" data-position-y="${i}"></div>`;
+            newCol.querySelector('.box').innerHTML += svg;
             newRow.append(newCol);
         }
         board.querySelector(".board__body").append(newRow);
@@ -205,8 +218,21 @@ function updateBoard(board) {
     updatePuntuacion(board);
 }
 
+function resetAllHeader(board) {
+    const title = board.querySelector(".board__header .title");
+    const titleSpan = board.querySelector(".board__header .title span");
+    titleSpan.textContent = '¡Empezamos!';
+    const titleSvg = board.querySelector(".board__header .title svg");
+
+    if (titleSvg) {
+        title.removeChild(titleSvg);
+    }
+
+}
+
 function newBoard(board) {
     resetAllLetDependencies();
+    resetAllHeader(board);
     updateBoard(board);
     board.querySelector('.nuevo__juego').setAttribute('disabled', 'disabled');
     playerWinner = null;
@@ -245,22 +271,14 @@ boards.forEach((board) => {
             // Cambiamos el valor de colorPrimary para que actualice la url de la imagen
             colorPrimary = !colorPrimary;
 
-            // Cambiando la imagen del Header
-            const titleImage = board.querySelector(".board__header .title img");
-
-            if (titleImage) {
-                if (isTurnX) {
-                    titleImage.src = `${pathImage}x${colorPrimary ? "" : "2"}.png`;
-                } else {
-                    titleImage.src = `${pathImage}o${colorPrimary ? "" : "2"}.png`;
-                }
-            }
-
             // Cambiamos los svg de la puntuación
             const svgPuntuacion = board.querySelectorAll(".board__footer--item");
             svgPuntuacion.forEach((item) => {
                 item.querySelector("svg")?.classList?.toggle("secundary");
             });
+
+            // Cambiando el svg del Header
+            updateTitleBoard(board);
 
             updateBoard(board);
 
